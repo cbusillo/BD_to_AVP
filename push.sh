c3e3c0c1e1f68cc7f5ad2b88f2012a5b8da2d532
Chris Busillo <info@shinycomputers.com>
2024-06-12 20:29:23 -0400

    fix env var and duplicate line

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" == "release" ]; then
+  SOURCE_BRANCH="prerelease"
+  DESTINATION_BRANCH="release"
+elif [ "$1" == "prerelease" ]; then
+  SOURCE_BRANCH="master"
+  DESTINATION_BRANCH="prerelease"
+else
+  echo "Please provide the destination branch name as an argument."
+  exit 1
+fi
+
+git checkout "$SOURCE_BRANCH"
+git pull origin "$SOURCE_BRANCH"
+git checkout "$DESTINATION_BRANCH"
+git merge "$SOURCE_BRANCH" -m "merge "$SOURCE_BRANCH" into "$DESTINATION_BRANCH""
+git push
+git checkout "$SOURCE_BRANCH"
+git pull origin "$SOURCE_BRANCH"
+git merge "$DESTINATION_BRANCH" -m "merge $DESTINATION_BRANCH into $SOURCE_BRANCH"
+git push
