3d89e73b872147b3624ed83abe690501f81a6333
Chris Busillo <info@shinycomputers.com>
2024-06-15 17:58:59 -0400

    allow continue with transcoding selected.

--- bd_to_avp/modules/audio.py
+++ bd_to_avp/modules/audio.py
@@ -9,7 +9,7 @@ from bd_to_avp.modules.command import run_ffmpeg_print_errors
 def transcode_audio(input_path: Path, transcoded_audio_path: Path, bitrate: int):
     audio_input = ffmpeg.input(str(input_path))
     audio_transcoded = ffmpeg.output(
-        audio_input,
+        audio_input["a"],
         str(f"file:{transcoded_audio_path}"),
         acodec="aac_at",
         audio_bitrate=f"{bitrate}k",
@@ -18,11 +18,13 @@ def transcode_audio(input_path: Path, transcoded_audio_path: Path, bitrate: int)
 
 
 def create_transcoded_audio_file(original_audio_path: Path, output_folder: Path) -> Path:
+    trancoded_audio_path = output_folder / f"{output_folder.stem}_audio_AAC.mov"
     if config.transcode_audio and config.start_stage.value <= Stage.TRANSCODE_AUDIO.value:
-        trancoded_audio_path = output_folder / f"{output_folder.stem}_audio_AAC.mov"
         transcode_audio(original_audio_path, trancoded_audio_path, config.audio_bitrate)
         if not config.keep_files:
             original_audio_path.unlink(missing_ok=True)
         return trancoded_audio_path
     else:
+        if config.transcode_audio and trancoded_audio_path.exists():
+            return trancoded_audio_path
         return original_audio_path
